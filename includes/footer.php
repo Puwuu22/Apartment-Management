1b7feccf832d1001d1419df34393e7fce10abf78
Puwuu22 <22521235@gm.uit.edu.vn>
2024-11-02 12:52:23 +0700

    update code

--- includes/footer.php
+++ includes/footer.php
@@ -1,26 +1,8 @@
-<style>
-    body {
-        display: flex;
-        flex-direction: column;
-        min-height: 100vh;
-    }
-
-    .content-wrapper {
-        flex: 1 0 auto;
-    }
-
-    .sticky-footer {
-        flex-shrink: 0;
-    }
-</style>
-
 <!-- Content Wrapper -->
 <div class="content-wrapper">
     <!-- Main Content -->
     <div>
-        <!-- Container Fluid -->
         <div>
-            <!-- Your content goes here -->
         </div>
     </div>
 </div>
